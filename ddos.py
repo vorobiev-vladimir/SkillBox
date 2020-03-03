dd2c9bbec607fa06feee5c7f4177f0ae699be0f4
vorobiev-vladimir <vorobiev1984@gmail.com>
2020-03-03 15:13:54 +0300

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,19 @@
+from requests import get
+
+urls = [
+    {'link' : 'https://newdaynews.ru/'},
+    {'link' : 'https://moskva.mts.ru/personal'},
+    {'link' : 'http://golang.com'},
+    {'link' : 'https://www.python.org'},
+    {'link' : 'https://www.google.ru'}
+]
+
+for url in urls:
+    for i in range(1, 101):
+        code = get(url['link']).status_code
+
+        if url.get(code) == None:
+            url[code] = 1
+        else:
+            url[code] += 1
+    print(url)
